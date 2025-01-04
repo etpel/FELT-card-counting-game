23cc52838ac90f0a48165bd64a46fc4c6c755471
etpel <65419252+etpel@users.noreply.github.com>
2025-01-04 23:01:14 +0000

    rearranging

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,24 @@
-import { makeShoe, rand } from '../util'
+/* File: ./src/actions/index.js */
+import { makeShoe } from '../util'
 
 export const NEW_GAME = 'NEW_GAME'
-export const DEAL_CARDS = 'DEAL_CARDS'
-export const TOGGLE_COUNT = 'TOGGLE_COUNT'
-
-// NEW
 export const DEAL_INITIAL = 'DEAL_INITIAL'
+export const TOGGLE_COUNT = 'TOGGLE_COUNT'
 export const PLAYER_CHOICE = 'PLAYER_CHOICE'
 
 export const newGame = () => ({
   type: NEW_GAME,
   shoe: makeShoe(8),
-  rand: rand(),
 })
 
-export const deal = () => ({
-  type: DEAL_CARDS,
-  rand: rand(),
+export const dealInitial = () => ({
+  type: DEAL_INITIAL
 })
 
 export const toggleCount = () => ({
   type: TOGGLE_COUNT,
 })
 
-// NEW
-export const dealInitial = () => ({
-  type: DEAL_INITIAL
-})
-
-// NEW
 export const playerChoice = (choice) => ({
   type: PLAYER_CHOICE,
   choice, // 'split', 'hit', or 'double'
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,47 +4,74 @@ import Header from './Header'
 
 const Game = ({ game, actions }) => {
   const {
-    shoe, idx, rand, count, is_visible,
-    playerHand, dealerUpCard, recommendedAction, userResult,
+    shoe,
+    idx,
+    count,
+    is_visible,
+    playerHand,
+    dealerUpCard,
+    userResult,
   } = game
 
-  // For the old random dealing
-  const idxEnd = idx + rand
-  const cards = shoe.slice(idx, idxEnd)
-  const isOver = idxEnd >= shoe.length
-
-  // --- RENDER INITIAL HANDS IF THEY EXIST ---
-  const renderHand = (hand) => {
-    return hand.map((c, i) => (
-      <img
-        key={i}
-        alt={c}
-        className='mr1'
-        src={`${process.env.PUBLIC_URL}/img/cards/${c}.svg`}
-        style={{ width: 80, height: 111 }}
-      />
-    ))
-  }
+  const renderCard = (card) => (
+    <img
+      key={card}
+      alt={card}
+      className='mr1'
+      src={`${process.env.PUBLIC_URL}/img/cards/${card}.svg`}
+      style={{ width: 80, height: 111 }}
+    />
+  )
 
   return (
     <div className='p3 mx-auto' style={{ maxWidth: 600 }}>
       <Header />
 
-      {/* Show the three newly dealt cards if any */}
+      {/* Dealer's Upcard */}
       <h2>Dealer's Upcard:</h2>
-      {dealerUpCard && renderHand([dealerUpCard])}
+      {dealerUpCard && renderCard(dealerUpCard)}
 
+      {/* Player's Hand */}
       <h2>Player's Hand:</h2>
-      {playerHand && renderHand(playerHand)}
+      {playerHand.map(renderCard)}
 
-      {/* Show the user result after choice */}
+      {/* Show result if user clicked Split/Hit/Double */}
       {userResult && (
         <p className="mt2 mb2 bold">
           {userResult}
         </p>
       )}
 
-      {/* NEW: Buttons for user actions (split, hit, double) */}
+      {/* Buttons to start new game or deal initial 3 cards */}
+      <div className='mb3'>
+        <button
+          className='btn btn-primary bg-black mr2'
+          onClick={actions.newGame}
+        >
+          Reset (New Game)
+        </button>
+        <button
+          className='btn btn-primary bg-purple'
+          onClick={actions.dealInitial}
+        >
+          Deal Initial (2 Player, 1 Dealer)
+        </button>
+      </div>
+
+      {/* Toggle count */}
+      <div className='mb3'>
+        <button
+          className='btn btn-primary bg-red'
+          onClick={actions.toggleCount}
+        >
+          {is_visible ? 'Hide' : 'Show'} running count
+        </button>
+        {is_visible && (
+          <span className='ml2 h3 bold align-middle'>{count}</span>
+        )}
+      </div>
+
+      {/* Move the player actions to the bottom */}
       <div className='mb3'>
         <button
           className='btn btn-primary bg-blue mr2'
@@ -66,61 +93,8 @@ const Game = ({ game, actions }) => {
         </button>
       </div>
 
-      {/* Buttons to start over, or deal initial, or old functionality */}
-      <div className='mb2'>
-        <button
-          className='btn btn-primary bg-black mr2'
-          onClick={actions.newGame}
-        >
-          Reset (New Game)
-        </button>
-        <button
-          className='btn btn-primary bg-purple mr2'
-          onClick={actions.dealInitial}
-        >
-          Deal Initial (2 Player, 1 Dealer)
-        </button>
-        <button
-          className='btn btn-primary bg-black'
-          disabled={isOver}
-          onClick={actions.deal}
-        >
-          More cards →
-        </button>
-      </div>
-
-      {/* Original Count Visibility */}
-      <div className='mb2'>
-        <button
-          className='btn btn-primary bg-red'
-          onClick={actions.toggleCount}
-          style={{ width: 210 }}
-        >
-          {is_visible ? 'Hide' : 'Show'} running count
-        </button>
-        {is_visible && (
-          <span className='ml2 h3 bold align-middle'>{count}</span>
-        )}
-      </div>
-
-      {/* Show old or leftover stuff if you want */}
-      <div className='mb3'>
-        {cards.map((c, i) => (
-          <img
-            key={i}
-            alt={c}
-            className='mr1'
-            src={`${process.env.PUBLIC_URL}/img/cards/${c}.svg`}
-            style={{ width: 80, height: 111 }}
-          />
-        ))}
-      </div>
-
       <p className='h5'>
-        {isOver
-          ? `Nice! You just went through ${shoe.length} cards 🎉`
-          : `Cards seen: ${idxEnd} (${shoe.length - idxEnd} remaining)`
-        }
+        Cards seen: {idx} ({shoe.length - idx} remaining)
       </p>
 
       <Footer />
